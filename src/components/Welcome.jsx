6b5c0de78fd8885d4125d54e6209fdfc661c0b40
kevinmutai <mutaikevinkip.05@gmail.com>
2025-12-29 22:36:08 +0300

    install and set up gsap

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useRef } from 'react'
+
+const renderText = (text) => {}
 
 const Welcome = () => {
+
+  const titleRef = useRef(null)
+  const subtitleRef = useRef(null)
+
   return (
-    <div>Welcome</div>
+    <section id="welcome">
+      <p ref={subtitleRef}>Hey, I'm Kevin! Welcome to my</p>
+      <h1 ref={titleRef} className='mt-7'>portfolio</h1>
+
+      <div className='small-screen'>
+        <p>This portfolio is Mac-themed and is, therefore, designed for desktop/tablet screens only. </p>
+      </div>
+    </section>
   )
 }
 
