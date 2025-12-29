f9a2e8827db06b2111be3a72b1b77d664df42129
kevinmutai <mutaikevinkip.05@gmail.com>
2025-12-29 15:13:35 +0300

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { navLinks, navIcons } from '../../constants'
+import dayjs from "dayjs"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt='logo'/>
+            <p className='font-bold'>Kevin's Portfolio</p>
+            {navLinks.map((item) => (
+                <li key={item.id}>
+                    <p>{item.name}</p>
+                </li>
+            ))}
+        </div>
+        <div>
+            <ul>
+                {
+                    navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} className='icon-hover'/>
+                        </li>
+                    ))
+                }
+            </ul>
+            <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
